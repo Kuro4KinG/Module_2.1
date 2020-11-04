6839172b7fe8d729a1e987c51a3dad3d423ce2b4
Kurochkin Vladimir <kuro4king@gmail.com>
2020-11-04 16:53:16 +0200

    Изменил void findDup() на boolean findDup()

--- src/main/java/com/kuro4king/practice2/Ex2/FindDuplicates.java
+++ src/main/java/com/kuro4king/practice2/Ex2/FindDuplicates.java
@@ -6,40 +6,17 @@ import java.util.Set;
 public class FindDuplicates {
 
     FindDuplicates(int[] array) {
-        showArray(array);
         findDup(array);
     }
 
-    // Метод выводит массив
-    private void showArray(int[] array) {
-        System.out.print("Array = [ ");
-        for (int i : array) {
-            System.out.print(i + " ");
-        }
-        System.out.print("]");
-        System.out.println();
-    }
-
-    // Метод ищет дубликаты в массиве и выводит их на экран
-    private void findDup(int[] array) {
-        boolean result = false;
-        int countDup = 1;
-        int dupNumber = 0;
+    // Метод проверяет массив на наличие дубликатов
+    private boolean findDup(int[] array) {
         Set set = new HashSet();
 
-        // Поиск дубликатов
         for (int j : array) {
-            if (!set.add(j)) {
-                result = true;
-                dupNumber = j;
-                countDup++;
-            }
+            if (!set.add(j))
+                return true;
         }
-        System.out.println("Result: " + result);
-        if (result)
-            System.out.println("Число " + dupNumber + " повторяется " + countDup + " раза.");
-        else System.out.println("Дубликатов нет");
-        System.out.println();
-
+        return false;
     }
 }
