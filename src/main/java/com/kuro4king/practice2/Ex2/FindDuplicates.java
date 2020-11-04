195fd7586520af09931aaaa08afa23e3a576595d
Kurochkin Vladimir <kuro4king@gmail.com>
2020-11-04 18:34:01 +0200

    Удалил закомментированный код и убрал вывод строкв консоль.

--- src/main/java/com/kuro4king/practice2/Ex2/FindDuplicates.java
+++ src/main/java/com/kuro4king/practice2/Ex2/FindDuplicates.java
@@ -1,62 +1,21 @@
 package com.kuro4king.practice2.Ex2;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class FindDuplicates {
 
     FindDuplicates(int[] array) {
-        showArray(array);
-        System.out.println("Result = " + findDup(array));
-        System.out.println();
+        findDup(array);
     }
 
-    // Метод выводит массив
-    private void showArray(int[] array) {
-        System.out.print("Array = [ ");
-        for (int i : array) {
-            System.out.print(i + " ");
-        }
-        System.out.print("]");
-        System.out.println();
-    }
-
-    // Метод ищет дубликаты в массиве и выводит их на экран
     private boolean findDup(int[] array) {
-        boolean result = false;
         Set set = new HashSet();
 
-        // Поиск дубликатов
         for (int j : array) {
             if (!set.add(j))
-                result = true;
+                return true;
         }
-        return result;
+        return false;
 
     }
-
-
-
-    /*// Метод ищет дубликаты в массиве и выводит их на экран
-    private void findDup(int[] array) {
-        boolean result = false;
-        int countDup = 1;
-        int dupNumber = 0;
-        Set set = new HashSet();
-
-        // Поиск дубликатов
-        for (int j : array) {
-            if (!set.add(j)) {
-                result = true;
-                dupNumber = j;
-                countDup++;
-            }
-        }
-        System.out.println("Result: " + result);
-        if (result)
-            System.out.println("Число " + dupNumber + " повторяется " + countDup + " раза.");
-        else System.out.println("Дубликатов нет");
-        System.out.println();
-
-    }*/
 }
